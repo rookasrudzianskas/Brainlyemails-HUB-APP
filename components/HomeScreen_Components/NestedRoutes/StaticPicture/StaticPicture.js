a9401a97422308abbc1a55038974e91f8102f4f0
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 22:05:04 +0200

    Rokas

--- components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
+++ components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
@@ -8,17 +8,29 @@ const StaticPicture = () => {
     const [scrollPosition, setScrollPosition] = useRecoilState(appScrollPosition);
     setScrollPosition(scrollY);
     console.log(scrollY);
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => {
+        if(scrollY > 2680) {
+            setShow(true);
+        }
+    }
+
+    useEffect(() => {
+        handleShow();
+    }, [scrollY]);
+
 
     return (
         <div className="flex justify-center overflow-y-hide">
-            {/*{show && (*/}
+            {show && (
                 <div className="absolute">
                     <div className="fixed bottom-0 left-[28rem] px-80 py-64 bg-red-500">
                         <p>This is epic</p>
                     </div>
                     {/*<img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>*/}
                 </div>
-            {/*// )}*/}
+            )}
 
             {/*<div className="App">*/}
             {/*    {Array(200)*/}
