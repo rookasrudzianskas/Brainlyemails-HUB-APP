a96b0672dfa156e72d67089ca4cbdfeb8a67a9f2
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 21:50:45 +0200

    Rokas

--- components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/FirstSide/FirstSide.js
+++ components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/FirstSide/FirstSide.js
@@ -3,10 +3,20 @@ import TextComponent from "../SmallerComponents/TextComponent";
 
 const FirstSide = () => {
     return (
-        <div className="bg-red-500 flex-1 overflow-y-scroll scrollbar-hide h-screen flex flex-col items-center justify-center">
-            <TextComponent />
-            <TextComponent />
-            <TextComponent />
+        <div className="bg-red-500 flex-1 overflow-y-scroll scrollbar-hide h-screen ">
+            {/* @TODO to make it snap*/}
+            <div className="mb-96">
+
+            </div>
+            <div className="flex flex-col items-center justify-center  space-y-[30rem]">
+                <TextComponent />
+                <TextComponent />
+                <TextComponent />
+            </div>
+
+            <div className="mb-96">
+
+            </div>
         </div>
     );
 };
