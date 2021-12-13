c0903a161d290c6c16d16d7b5dd8f9990d1accc9
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 16:25:11 +0200

    ROkas

--- components/HomeScreen_Components/NestedRoutes/ScrollViewWithDetails/ScrollViewWithDetails.js
+++ components/HomeScreen_Components/NestedRoutes/ScrollViewWithDetails/ScrollViewWithDetails.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import Code4Times from "../../../CodeSnippets/Code4Times";
 import {CODE} from "../../../../data/CodeCards/code";
+import FadeIn from "react-fade-in";
 
 const ScrollViewWithDetails = () => {
 
@@ -11,29 +12,37 @@ const ScrollViewWithDetails = () => {
 
     return (
         <div className="flex justify-center flex-col items-center max-h-xl overflow-y-scroll">
-            <div className=" max-w-4xl p-10 pl-16">
-                <span className="text-7xl font-bold">
-                    Emails usually have levels of navigation that control child views.
-                </span>
-            </div>
+            <FadeIn>
+                <div className=" max-w-4xl p-10 pl-16">
+                    <span className="text-7xl font-bold">
+                        Emails usually have levels of navigation that control child views.
+                    </span>
+                </div>
+            </FadeIn>
 
-            <div className=" max-w-4xl p-10 pl-16">
-                <span className="text-7xl font-bold">
-                    Not only are these components pretty much always coupled to URL segments...
-                </span>
-            </div>
+            <FadeIn>
+                <div className=" max-w-4xl p-10 pl-16">
+                    <span className="text-7xl font-bold">
+                        Not only are these components pretty much always coupled to URL segments...
+                    </span>
+                </div>
+            </FadeIn>
 
-            <div className=" max-w-4xl p-10 pl-16">
-                <span className="text-7xl font-bold">
-                    ...they’re also the semantic boundary of data loading and code splitting.
-                </span>
-            </div>
+            <FadeIn>
+                <div className=" max-w-4xl p-10 pl-16">
+                    <span className="text-7xl font-bold">
+                        ...they’re also the semantic boundary of data loading and code splitting.
+                    </span>
+                </div>
+            </FadeIn>
 
-            <div className=" max-w-4xl p-10 pl-16">
-                <span className="text-7xl font-bold">
-                    Hover or tap the buttons to see how they’re all related
-                </span>
-            </div>
+            <FadeIn>
+                <div className=" max-w-4xl p-10 pl-16">
+                    <span className="text-7xl font-bold">
+                        Hover or tap the buttons to see how they’re all related
+                    </span>
+                </div>
+            </FadeIn>
 
             <div className="mt-56 flex flex-col items-center">
                 <p className="text-7xl animate-bounce cursor-pointer">👇</p>
