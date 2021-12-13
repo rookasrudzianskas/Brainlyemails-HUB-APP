3d3622bcf2d654581b85dbaaf0cca9ef3f833b4c
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 21:36:56 +0200

    Rokas

--- components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/SecondSide/SecondSide.js
+++ components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/SecondSide/SecondSide.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SecondSide = () => {
     return (
-        <div className="bg-blue-500 flex-1 overflow-y-scroll scrollbar-hide h-screen">
+        <div className="bg-blue-500 flex-1 overflow-y-hide scrollbar-hide h-screen">
 
         </div>
     );
