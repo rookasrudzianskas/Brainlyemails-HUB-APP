ab2ca8eed4a6d5880a93e22492807d2a1f770ca9
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 16:42:55 +0200

    Rokas

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -24,6 +24,7 @@ const HomeScreen = () => {
             <OverallNestedRoute />
             <BrainlyemailsNestedRoutes />
             <NotesAboutIt />
+            <WithBrainlyemailsAndWithout />
         </div>
     );
 };
