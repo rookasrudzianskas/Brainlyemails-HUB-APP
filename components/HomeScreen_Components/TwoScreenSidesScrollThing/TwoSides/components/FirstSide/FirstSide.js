46658eba9801cb030fde8eff423018c1b2236848
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 21:45:02 +0200

    Rokas

--- components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/FirstSide/FirstSide.js
+++ components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/FirstSide/FirstSide.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import TextComponent from "../SmallerComponents/TextComponent";
 
 const FirstSide = () => {
     return (
-        <div className="bg-red-500 flex-1 overflow-y-scroll scrollbar-hide h-screen">
-
+        <div className="bg-red-500 flex-1 overflow-y-scroll scrollbar-hide h-screen flex flex-col items-center justify-center">
+            <TextComponent />
+            <TextComponent />
+            <TextComponent />
         </div>
     );
 };
--- components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/SmallerComponents/TextComponent/TextComponent.js
+++ components/HomeScreen_Components/TwoScreenSidesScrollThing/TwoSides/components/SmallerComponents/TextComponent/TextComponent.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const TextComponent = () => {
     return (
-        <div className="">
-
+        <div className="flex items-center justify-center">
+            <div className="max-w-xl">
+                <p className="text-7xl font-bold text-white">
+                    It’s so simple it’s kind of silly. Just make a form...
+                </p>
+            </div>
         </div>
     );
 };
