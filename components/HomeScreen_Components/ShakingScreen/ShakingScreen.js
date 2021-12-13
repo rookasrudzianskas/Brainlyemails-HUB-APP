34a319e59588c0d607ccb3cd41e17c95b72d6177
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 22:41:39 +0200

    Rokas

--- components/HomeScreen_Components/ShakingScreen/ShakingScreen.js
+++ components/HomeScreen_Components/ShakingScreen/ShakingScreen.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const ShakingScreen = () => {
+    const linearyaxis = useRef();
+
     return (
         <div className="">
-            <div>
+            <div className="hover:animate-ping">
                 <img src="https://brainlyemails-storage174941-staging.s3.us-east-1.amazonaws.com/busted.jpeg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGUaDGV1LWNlbnRyYWwtMSJGMEQCIGhkXdHGEoK9T0Tuvuxf3mb0%2FYijdq%2Fxqp1yf%2B0zyMacAiAatU1ONvky4frwle4Rf%2Feyl%2BSfiAmYYVWmBDy%2Fk3Lm%2BCr2AghOEAAaDDk4NjY5ODg0MDYxOCIMSHuwgdrXh%2B7%2FJS%2FIKtMCmgMU5EpDW53kwSehyjWeNXXQ6uRUEFmDkd4y44%2Bmw2m%2BbJ12LYMQ6eAw1sGb%2F2vUChnbhDVBARrJau628jRiVABN9jasaQnFT6X6QGp7om6GrTlh6Zw5h8DSHV2VIQ%2BfL3%2Fo4Cq4EJ2WrMkRgi2dMZtX3lBH824FQoKpbZR2o6U73%2Fpgc0aez%2BXjA517we%2FIo8VkY21FQ3yfmR0mNOkjIqgiDgTrRN8tyL3xd81vWuKksBS4TgEeXop6NPLQ1oMcgWM8dferTx1gPQnBngc0MOp3a6YNp0eGD6MSgfHMKZj6aj2WTfcwM0%2F9w0%2FpOEBu7ws8dEJA7Li9iHXGnw0cdOOJEmmsg5jsam6StpEe2%2Bv4cuAMWopIwZq7UnOYsDfhJLRVUuZz%2F%2BIO8necdlax8juO3t8VjPZBPKg%2BuYJNucSopM7hka3uG5T1lcwbGwK%2Fo2oOMLDc3o0GOrQCMvj6HBro%2BAPYoP2JQshBznaPMtBR2qUVKCX%2FUpbnWNvxJN8s40LZz9oawkQCJXfNuyV84JHMzZJtG3Fy6UkOvSdumLOKF26L4koX88TiTb17p3Np6tkFxp2kIiZ0IsWWAdUGT2R7jQ2poM6oHS18brne3ayiqnJZE%2F2RpXT5GiKkkl1%2FXrhLdCyKFyti%2FB%2B4qm4u0su6WTxsByaVA32s%2FNAObLaKhMPf3OiVIOdDADb2buf8Kijqr4Y0WHx6A3jMjsVLwZekklGW4fxaIiMcFEaOjHXUFRyYygkUjEaVvNA8daxAKjieCTGqdFRicYAMFEkuKbp52OlylDd3h547zTVcaf9cry%2F%2BURYK7dIYfUo9f4dkBz6oH%2Fu4Duw34cx%2FjiTVotTwfnwJFkLoDs5HdUOD9D4%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211213T203429Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA6LO6VPIVEKATQ2OK%2F20211213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=874f23fe0646355331a089354140bdd0a1f6a48587115fd68f41b656e4630abe" className="h-screen w-screen" alt=""/>
             </div>
         </div>
