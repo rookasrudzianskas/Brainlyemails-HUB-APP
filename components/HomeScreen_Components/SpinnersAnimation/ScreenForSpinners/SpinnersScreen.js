ee5f31cb534bfa91439d3f7d3d507381c069a114
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 18:29:53 +0200

    Rokas

--- components/HomeScreen_Components/SpinnersAnimation/ScreenForSpinners/SpinnersScreen.js
+++ components/HomeScreen_Components/SpinnersAnimation/ScreenForSpinners/SpinnersScreen.js
@@ -1,70 +1,21 @@
 import React from 'react';
 import PlayerOfSpinner from "../components/PlayerOfSpinner";
 import FadeIn from "react-fade-in";
+import ContinousComponent from "../components/ContinousComponent/ContinousComponent";
 
 const SpinnersScreen = () => {
     return (
         <div className="snap-y snap-mandatory w-screen h-screen bg-black">
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-28"} otherSide={"right-20"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-24"} otherSide={"left-[94rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"-top-[3rem]"} otherSide={"left-[12rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[48rem]"} otherSide={"left-80"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[48rem]"} otherSide={"left-[70rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[45rem]"} otherSide={"left-[95rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[37rem]"} otherSide={"left-32"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[49rem]"} otherSide={"left-[2rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[49rem]"} otherSide={"left-[53rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[37rem]"} otherSide={"left-[85rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[3rem]"} otherSide={"left-[75rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"-top-[1rem]"} otherSide={"left-[55rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[6rem]"} otherSide={"left-[36rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[15rem]"} otherSide={"left-[50rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[1rem]"} otherSide={"left-[90rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[28rem]"} otherSide={"left-[3rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[44rem]"} otherSide={"left-[35rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[40rem]"} otherSide={"left-[65rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[50rem]"} otherSide={"left-[87rem]"} />
-            </FadeIn>
-            <FadeIn>
-                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[30rem]"} otherSide={"left-[98rem]"} />
-            </FadeIn>
+            <ContinousComponent />
+            <div className="text-[12rem] font-bold text-white flex items-center justify-center flex-col opacity-60 hover:opacity-80 cursor-pointer transition duration-200 ease-in-out">
+                <div className="mt-[10rem]">
+                    Say good­bye to
+
+                </div>
+                <div className="">
+                    Spinnageddon
+                </div>
+            </div>
         </div>
     );
 };
---
+++ components/HomeScreen_Components/SpinnersAnimation/components/ContinousComponent/ContinousComponent.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import FadeIn from "react-fade-in";
+import PlayerOfSpinner from "../PlayerOfSpinner";
+
+const ContinousComponent = () => {
+    return (
+        <div>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-28"} otherSide={"right-20"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-24"} otherSide={"left-[94rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"-top-[3rem]"} otherSide={"left-[12rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[48rem]"} otherSide={"left-80"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[48rem]"} otherSide={"left-[70rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={200} width={200} frontBack={"top-[45rem]"} otherSide={"left-[95rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[37rem]"} otherSide={"left-32"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[49rem]"} otherSide={"left-[2rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[49rem]"} otherSide={"left-[53rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[37rem]"} otherSide={"left-[85rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[3rem]"} otherSide={"left-[75rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"-top-[1rem]"} otherSide={"left-[55rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={135} width={135} frontBack={"top-[6rem]"} otherSide={"left-[36rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[15rem]"} otherSide={"left-[50rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[1rem]"} otherSide={"left-[90rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[28rem]"} otherSide={"left-[3rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[44rem]"} otherSide={"left-[35rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[40rem]"} otherSide={"left-[65rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[50rem]"} otherSide={"left-[87rem]"} />
+            </FadeIn>
+            <FadeIn>
+                <PlayerOfSpinner position={"relative"} height={90} width={90} frontBack={"top-[30rem]"} otherSide={"left-[98rem]"} />
+            </FadeIn>
+        </div>
+    );
+};
+
+export default ContinousComponent;
