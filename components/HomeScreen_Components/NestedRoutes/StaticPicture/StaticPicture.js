49c258da0e6318c46f898fe4a60822d26767a17f
Rokas <rookas.rudzenskas@gmail.com>
2021-12-13 15:29:16 +0200

    Rokas

--- components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
+++ components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import useScrollPosition from "@react-hook/window-scroll";
 import {useRecoilState} from "recoil";
 import {appScrollPosition} from "../../../../contentManagement/atoms/webAppScrollPosition/webAppScrollPosition";
+import FadeIn from 'react-fade-in';
+
 
 const StaticPicture = () => {
     const scrollY = useScrollPosition(60 /*fps*/);
@@ -26,12 +28,14 @@ const StaticPicture = () => {
     return (
         <div className="flex justify-center overflow-y-hide">
             {show && (
-                <div className="absolute fade-in-down">
-                    <div className="fixed bottom-0 left-[28rem] px-80 py-64 bg-red-500">
-                        <p>This is epic</p>
+                <FadeIn>
+                    <div className="absolute fade-in-down">
+                        <div className="fixed bottom-0 left-[28rem] px-80 py-64 bg-red-500">
+                            <p>This is epic</p>
+                        </div>
+                        {/*<img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>*/}
                     </div>
-                    {/*<img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>*/}
-                </div>
+                </FadeIn>
             )}
 
             {/*<div className="App">*/}
