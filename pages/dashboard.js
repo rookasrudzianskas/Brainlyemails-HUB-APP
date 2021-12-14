7832a7c7f04607b0c2d609d421739dc3d5530869
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 17:32:37 +0200

    Rokas

---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="bg-black w-screen h-screen flex items-center justify-center">
+            <p className="text-7xl font-bold text-white">Welcome in! 🚀</p>
+        </div>
+    );
+};
+
+export default Dashboard;
