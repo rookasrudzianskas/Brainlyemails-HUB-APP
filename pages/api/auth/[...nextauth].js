f7fff7cabc42ad44d0c1362a7dcd8dd5ff58e093
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 17:45:37 +0200

    Rokas

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        GithubProvider({
+        GoogleProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
