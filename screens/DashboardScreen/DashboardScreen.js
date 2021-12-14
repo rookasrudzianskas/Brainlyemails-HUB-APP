94a297f655ce6829d8a9037f95d6dca170b36e7c
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 18:33:06 +0200

    Rokas

--- screens/DashboardScreen/DashboardScreen.js
+++ screens/DashboardScreen/DashboardScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from "../../components/DashboardComponents/Header/Header";
 
 const DashboardScreen = () => {
     return (
-        <div className="bg-black w-screen h-screen">
-
+        <div className="bg-black w-screen h-screen max-w-2xl">
+            <Header />
         </div>
     );
 };
