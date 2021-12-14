c5609603f7d6369e126fd1340c1036468bac4bfb
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 22:09:30 +0200

    Rokas

--- components/DashboardComponents/CombinedSending/CombinedSending.js
+++ components/DashboardComponents/CombinedSending/CombinedSending.js
@@ -4,23 +4,17 @@ const CombinedSending = () => {
     return (
         <div className="bg-transparent border border-1 border-gray-900 p-10 flex w-[50rem]">
             <div className="flex flex-row items-center">
-                {/*<div className="flex flex-grow">*/}
-                {/*    <p className="text-gray-300">Combined sending</p>*/}
-                {/*</div>*/}
-
-                {/*<div className="flex flex-col">*/}
-                {/*    <p className="text-white">30 days</p>*/}
-                {/*    <p className="text-white">0</p>*/}
-                {/*</div>*/}
-
-                <div className="flex text-white flex-grow">
-                    Rokas
+                <div className="flex flex-grow">
+                    <p className="text-gray-300 text-xl">Combined sending</p>
                 </div>
 
-                <div className="flex text-white">
-                    Rokas
+                <div className="flex flex-col ml-[18rem]">
+                    <p className="text-gray-400">30 days</p>
+                    <p className="text-gray-700 ml-[3rem] text-sm font-bold">0</p>
                 </div>
             </div>
+
+
         </div>
     );
 };
