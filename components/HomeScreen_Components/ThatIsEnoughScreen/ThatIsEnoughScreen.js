6b33e4cc74e36f5b9a29b9dc750b3cd7e2d1fef4
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 17:19:56 +0200

    ROkas

--- components/HomeScreen_Components/ThatIsEnoughScreen/ThatIsEnoughScreen.js
+++ components/HomeScreen_Components/ThatIsEnoughScreen/ThatIsEnoughScreen.js
@@ -4,18 +4,25 @@ const ThatIsEnoughScreen = () => {
     return (
         <div className="h-screen w-screen bg-black flex flex-col">
                 <div className="flex flex-col flex-1  items-center justify-center">
-                    <div className="flex">
+                    <div className="flex flex-col">
                         <div className="max-w-6xl cursor-pointer">
                             <h1 className="text-8xl font-bold text-white">That's probably enough for now. <span className=" text-transparent bg-clip-text bg-gradient-to-br from-green-300 to-green-400 cursor-pointer ">
                                         What are you waiting for?
                                    </span></h1>
                         </div>
+
+                        <div className="bg-blue-500 max-w-[8rem] py-4 flex items-center justify-center font-bold rounded-sm mt-10 hover:bg-blue-400 transition duration-150 ease-in-out cursor-pointer">Go Play!</div>
                     </div>
                 </div>
 
 
-                <div className="flex flex-col bg-yellow-500">
-                    <h1 className="text-3xl font-bold text-white">Rokas</h1>
+                <div className="flex flex-row bg-black items-center  px-10">
+                        <div className="flex flex-1">
+                            <img src="https://www.brainlyemails.com/images/img_14.png"  className="object-contain h-32 w-44" alt=""/>
+                        </div>
+                        <div>
+                            <h1>Icons</h1>
+                        </div>
                 </div>
 
         </div>
