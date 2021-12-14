e3bd8a23f8147b161f9fabc4b64aa235b87a1e81
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 17:07:20 +0200

    Rokas

--- components/HomeScreen_Components/ThatIsEnoughScreen/ThatIsEnoughScreen.js
+++ components/HomeScreen_Components/ThatIsEnoughScreen/ThatIsEnoughScreen.js
@@ -2,31 +2,36 @@ import React from 'react';
 
 const ThatIsEnoughScreen = () => {
     return (
-        <div className="h-screen w-screen bg-black flex items-center justify-center">
-            <div className="flex flex-col">
-                <div className="">
-                   <div className="flex flex-col">
-                       <div>
-                           <div className="max-w-6xl cursor-pointer">
-                               <h1 className="text-8xl font-bold text-white">That's probably enough for now. <span className=" text-transparent bg-clip-text bg-gradient-to-br from-green-300 to-green-400 cursor-pointer ">
-                                    What are you waiting for?
-                               </span></h1>
-                           </div>
+        <div className="h-screen w-screen bg-black">
+            {/*<div className="flex flex-col">*/}
+            {/*    <div className="">*/}
+            {/*       <div className="flex flex-col">*/}
+            {/*           <div>*/}
+            {/*               <div className="max-w-6xl cursor-pointer">*/}
+            {/*                   <h1 className="text-8xl font-bold text-white">That's probably enough for now. <span className=" text-transparent bg-clip-text bg-gradient-to-br from-green-300 to-green-400 cursor-pointer ">*/}
+            {/*                        What are you waiting for?*/}
+            {/*                   </span></h1>*/}
+            {/*               </div>*/}
 
-                       </div>
-                       <div className="bg-blue-500 max-w-[9rem] py-4 flex items-center justify-center rounded-sm mt-10 hover:bg-blue-400 transition duration-150 ease-in-out cursor-pointer">
-                           <span className="font-bold">Go Play!</span>
-                       </div>
-                   </div>
-
-                    <div className="flex flex-col justify-end">
-                        <h1 className="text-3xl font-bold text-white">Rokas</h1>
-                    </div>
+            {/*           </div>*/}
+            {/*           <div className="bg-blue-500 max-w-[9rem] py-4 flex items-center justify-center rounded-sm mt-10 hover:bg-blue-400 transition duration-150 ease-in-out cursor-pointer">*/}
+            {/*               <span className="font-bold">Go Play!</span>*/}
+            {/*           </div>*/}
+            {/*       </div>*/}
+            {/*<div className="flex flex-col ">*/}
+                <div className="flex flex-1 bg-red-500">
+                    <h1>Rokas</h1>
                 </div>
-            </div>
 
-
+                <div className="flex flex-col bg-yellow-500">
+                    <h1 className="text-3xl font-bold text-white">Rokas</h1>
+                </div>
+            {/*</div>*/}
+                {/*</div>*/}
         </div>
+
+
+        // </div>
     );
 };
 
