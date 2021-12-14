ec1ad1f7345883d2814e137d1eeb1ff536b7cbda
Rokas <rookas.rudzenskas@gmail.com>
2021-12-14 14:01:39 +0200

    Rokas

--- components/HomeScreen_Components/TwoScrollableScreensConnected/FirstScreen/components/Bottom/Bottom.js
+++ components/HomeScreen_Components/TwoScrollableScreensConnected/FirstScreen/components/Bottom/Bottom.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Bottom = () => {
     return (
-        <div className="bg-red-500 flex-1 overflow-x-hide scrollbar-hide x-screen">
-
+        <div className="bg-black flex-1 overflow-x-hide scrollbar-hide x-screen">
+            <div className="flex items-center justify-center mt-44">
+                <p className="text-8xl font-bold">Something very cool 🔥</p>
+            </div>
         </div>
     );
 };
--- components/HomeScreen_Components/TwoScrollableScreensConnected/FirstScreen/components/Top/Top.js
+++ components/HomeScreen_Components/TwoScrollableScreensConnected/FirstScreen/components/Top/Top.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ScrollViewWithDetails from "../../../../NestedRoutes/ScrollViewWithDetails";
 
 const Top = () => {
     return (
-        <div className="bg-blue-500 flex-1 overflow-x-hide scrollbar-hide x-screen">
-
+        <div className="bg-black flex-1 overflow-x-scroll scrollbar-hide x-screen text-white">
+            <ScrollViewWithDetails />
         </div>
     );
 };
