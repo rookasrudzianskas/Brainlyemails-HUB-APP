8095c9ee0a23baccd2a2487280b5e462cf484826
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 17:48:44 +0200

    Rokas

--- components/NewProject_Components/TwoSides/FirstSide/FirstSide.js
+++ components/NewProject_Components/TwoSides/FirstSide/FirstSide.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const FirstSide = () => {
     return (
-        <div className="bg-red-500 flex flex-1">
-            <div>
+        <div className="bg-red-500 flex flex-1 flex-col">
+            <div className="mt-5">
                 <h1>Logo</h1>
+            </div>
+
+            <div className="mt-2">
+                <h1 className="text-sm text-gray-300">Max size 400 kb.</h1>
             </div>
         </div>
     );
