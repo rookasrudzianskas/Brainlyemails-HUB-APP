7627871c575672b668466b1c69063a67a2644114
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 11:15:04 +0200

    Rokas

--- components/DashboardComponents/partials/dashboard/DashboardAvatars.js
+++ components/DashboardComponents/partials/dashboard/DashboardAvatars.js
@@ -9,24 +9,24 @@ function DashboardAvatars() {
     return (
         <ul className="flex flex-wrap justify-center sm:justify-start mb-8 sm:mb-0 -space-x-3 -ml-px">
             <li>
-                {/*<Link className="block" to="/">*/}
+                <div className="block" >
                     <img className="w-9 h-9 rounded-full" src={User01} width="36" height="36" alt="User 01" />
-                {/*</Link>*/}
+                </div>
             </li>
             <li>
-                {/*<Link className="block" to="/">*/}
+                <div className="block" >
                     <img className="w-9 h-9 rounded-full" src={User02} width="36" height="36" alt="User 02" />
-                {/*</Link>*/}
+                </div>
             </li>
             <li>
-                {/*<Link className="block" to="/">*/}
+                <div className="block" >
                     <img className="w-9 h-9 rounded-full" src={User03} width="36" height="36" alt="User 03" />
-                {/*</Link>*/}
+                </div>
             </li>
             <li>
-                {/*<Link className="block" to="/">*/}
+                <div className="block" >
                     <img className="w-9 h-9 rounded-full" src={User04} width="36" height="36" alt="User 04" />
-                {/*</Link>*/}
+                </div>
             </li>
             <li>
                 <button className="flex justify-center items-center w-9 h-9 rounded-full bg-white border border-gray-200 hover:border-gray-300 text-indigo-500 shadow-sm transition duration-150 ml-2">
