2e601e239e09d8295a02c9f504845e87ae362fd7
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 10:38:04 +0200

    Rokas

--- screens/DashboardScreen/DashboardScreen.js
+++ screens/DashboardScreen/DashboardScreen.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "../../components/DashboardComponents/Header/Header";
 import Feed from "../../components/DashboardComponents/Feed";
+import Dashboard from "../../components/DashboardComponents/pages/Dashboard";
+import './css/style.scss';
+
+import './charts/ChartjsConfig';
 
 const DashboardScreen = () => {
+
+    // const location = useLocation();
+
+    // useEffect(() => {
+    //     document.querySelector('html').style.scrollBehavior = 'auto'
+    //     window.scroll({ top: 0 })
+    //     document.querySelector('html').style.scrollBehavior = ''
+    // }, [location.pathname]); // triggered on route change
+
     return (
-        <div className="bg-black h-screen">
+        <div className="">
             {/*<Header />*/}
-            <Feed />
+            <Dashboard />
         </div>
     );
 };
