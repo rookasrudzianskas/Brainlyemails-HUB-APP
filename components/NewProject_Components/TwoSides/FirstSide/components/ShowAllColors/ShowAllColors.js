494fab2b48c618a63675893ead0656b0a70febc9
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 21:28:28 +0200

    Rokas

--- components/NewProject_Components/TwoSides/FirstSide/components/ShowAllColors/ShowAllColors.js
+++ components/NewProject_Components/TwoSides/FirstSide/components/ShowAllColors/ShowAllColors.js
@@ -160,7 +160,7 @@ const ShowAllColors = () => {
                             </div>
                             <div className="ml-[60px] flex flex-row">
                                 <HexColorPicker className="w-[10rem] h-[10rem]" color={contentBorderColor} onChange={setContentBorderColor} />
-                                <DoneIcon  onClick={onColorButtonClickFirst} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                                <DoneIcon  onClick={onColorButtonClickSecond} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
                             </div>
 
 
@@ -172,7 +172,7 @@ const ShowAllColors = () => {
 
                     </div>
                 ): (
-                    <div onClick={onColorButtonClickFirst} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                    <div onClick={onColorButtonClickSecond} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
                         <div className="">
                             <div className={`h-7 w-7 bg-[${bgContentBorderColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
                         </div>
@@ -211,7 +211,7 @@ const ShowAllColors = () => {
                             </div>
                             <div className="ml-[60px] flex flex-row">
                                 <HexColorPicker className="w-[10rem] h-[10rem]" color={buttonTextColor} onChange={setButtonTextColor} />
-                                <DoneIcon  onClick={onColorButtonClickFirst} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                                <DoneIcon  onClick={onColorButtonClickThird} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
                             </div>
 
 
@@ -223,7 +223,7 @@ const ShowAllColors = () => {
 
                     </div>
                 ): (
-                    <div onClick={onColorButtonClickFirst} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                    <div onClick={onColorButtonClickThird} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
                         <div className="">
                             <div className={`h-7 w-7 bg-[${bgButtonTextColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
                         </div>
@@ -240,6 +240,210 @@ const ShowAllColors = () => {
                 )}
             </div>
 
+            {/*Text color*/}
+
+            <div className="mt-5">
+                <h1 className="text-sm font-bold text-indigo-500">Text color</h1>
+            </div>
+
+            <div>
+                {show4 ? (
+                    <div
+                        // onClick={onColorButtonClickFirst}
+                        className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover: cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className="mb-4 flex flex-row mt-3">
+                                <div className="">
+                                    <div className={`h-9 w-9 ${bgTextColor} rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                                </div>
+                                <input
+                                    className="shadow appearance-none border rounded w-[16rem] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="username" type="text" value={textColor} />
+                            </div>
+                            <div className="ml-[60px] flex flex-row">
+                                <HexColorPicker className="w-[10rem] h-[10rem]" color={textColor} onChange={setTextColor} />
+                                <DoneIcon  onClick={onColorButtonClickFourth} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                            </div>
+
+
+                            {/*<div className="text-right" onClick={onColorButtonClickFirst}>*/}
+                            {/*    <p className="text-sm font-bold text-indigo-500  mt-3 hover:underline transition duration-150 ease-in-out">I am happy with my color 🚀</p>*/}
+                            {/*</div>*/}
+
+                        </div>
+
+                    </div>
+                ): (
+                    <div onClick={onColorButtonClickFourth} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className={`h-7 w-7 bg-[${bgTextColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                        </div>
+                        <p className="text-gray-800 text-sm" >{textColor}</p>
+
+                        {/*{show ? (*/}
+                        {/*) : (*/}
+                        {/*    <div>*/}
+
+                        {/*    </div>*/}
+                        {/*)}*/}
+
+                    </div>
+                )}
+            </div>
+
+            {/*Title text color*/}
+
+            <div className="mt-5">
+                <h1 className="text-sm font-bold text-indigo-500">Title text color</h1>
+            </div>
+
+            <div>
+                {show5 ? (
+                    <div
+                        // onClick={onColorButtonClickFirst}
+                        className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover: cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className="mb-4 flex flex-row mt-3">
+                                <div className="">
+                                    <div className={`h-9 w-9 ${bgTitleTextColor} rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                                </div>
+                                <input
+                                    className="shadow appearance-none border rounded w-[16rem] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="username" type="text" value={titleTextColor} />
+                            </div>
+                            <div className="ml-[60px] flex flex-row">
+                                <HexColorPicker className="w-[10rem] h-[10rem]" color={titleTextColor} onChange={setTitleTextColor} />
+                                <DoneIcon  onClick={onColorButtonClickFifth} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                            </div>
+
+
+                            {/*<div className="text-right" onClick={onColorButtonClickFirst}>*/}
+                            {/*    <p className="text-sm font-bold text-indigo-500  mt-3 hover:underline transition duration-150 ease-in-out">I am happy with my color 🚀</p>*/}
+                            {/*</div>*/}
+
+                        </div>
+
+                    </div>
+                ): (
+                    <div onClick={onColorButtonClickFifth} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className={`h-7 w-7 bg-[${bgTitleTextColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                        </div>
+                        <p className="text-gray-800 text-sm" >{titleTextColor}</p>
+
+                        {/*{show ? (*/}
+                        {/*) : (*/}
+                        {/*    <div>*/}
+
+                        {/*    </div>*/}
+                        {/*)}*/}
+
+                    </div>
+                )}
+            </div>
+
+
+            {/*Muted text color*/}
+
+            <div className="mt-5">
+                <h1 className="text-sm font-bold text-indigo-500">Muted text color</h1>
+            </div>
+
+            <div>
+                {show6 ? (
+                    <div
+                        // onClick={onColorButtonClickFirst}
+                        className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover: cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className="mb-4 flex flex-row mt-3">
+                                <div className="">
+                                    <div className={`h-9 w-9 ${bgMutedTextColor} rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                                </div>
+                                <input
+                                    className="shadow appearance-none border rounded w-[16rem] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="username" type="text" value={mutedTextColor} />
+                            </div>
+                            <div className="ml-[60px] flex flex-row">
+                                <HexColorPicker className="w-[10rem] h-[10rem]" color={mutedTextColor} onChange={setMutedTextColor} />
+                                <DoneIcon  onClick={onColorButtonClickSixth} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                            </div>
+
+
+                            {/*<div className="text-right" onClick={onColorButtonClickFirst}>*/}
+                            {/*    <p className="text-sm font-bold text-indigo-500  mt-3 hover:underline transition duration-150 ease-in-out">I am happy with my color 🚀</p>*/}
+                            {/*</div>*/}
+
+                        </div>
+
+                    </div>
+                ): (
+                    <div onClick={onColorButtonClickSixth} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className={`h-7 w-7 bg-[${bgMutedTextColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                        </div>
+                        <p className="text-gray-800 text-sm" >{mutedTextColor}</p>
+
+                        {/*{show ? (*/}
+                        {/*) : (*/}
+                        {/*    <div>*/}
+
+                        {/*    </div>*/}
+                        {/*)}*/}
+
+                    </div>
+                )}
+            </div>
+
+            {/*Footer text color*/}
+
+            <div className="mt-5">
+                <h1 className="text-sm font-bold text-indigo-500">Footer text color</h1>
+            </div>
+
+            <div>
+                {show7 ? (
+                    <div
+                        // onClick={onColorButtonClickFirst}
+                        className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover: cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className="mb-4 flex flex-row mt-3">
+                                <div className="">
+                                    <div className={`h-9 w-9 ${bgFooterTextColor} rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                                </div>
+                                <input
+                                    className="shadow appearance-none border rounded w-[16rem] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="username" type="text" value={footerTextColor} />
+                            </div>
+                            <div className="ml-[60px] flex flex-row">
+                                <HexColorPicker className="w-[10rem] h-[10rem]" color={footerTextColor} onChange={setFooterTextColor} />
+                                <DoneIcon  onClick={onColorButtonClickSeventh} className="text-gray-400 ml-[35px] bg-white rounded-full hover:text-gray-500 cursor-pointer transition duration-150 ease-in-out" />
+                            </div>
+
+
+                            {/*<div className="text-right" onClick={onColorButtonClickFirst}>*/}
+                            {/*    <p className="text-sm font-bold text-indigo-500  mt-3 hover:underline transition duration-150 ease-in-out">I am happy with my color 🚀</p>*/}
+                            {/*</div>*/}
+
+                        </div>
+
+                    </div>
+                ): (
+                    <div onClick={onColorButtonClickSeventh} className={`flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out`}>
+                        <div className="">
+                            <div className={`h-7 w-7 bg-[${bgFooterTextColor}] rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100`}/>
+                        </div>
+                        <p className="text-gray-800 text-sm" >{footerTextColor}</p>
+
+                        {/*{show ? (*/}
+                        {/*) : (*/}
+                        {/*    <div>*/}
+
+                        {/*    </div>*/}
+                        {/*)}*/}
+
+                    </div>
+                )}
+            </div>
 
         </div>
     );
