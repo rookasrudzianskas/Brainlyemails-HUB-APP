bd2b66767a61a4d57bb3d30748a1ea30895e3818
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 18:16:32 +0200

    Rokas

--- components/NewProject_Components/TwoSides/FirstSide/FirstSide.js
+++ components/NewProject_Components/TwoSides/FirstSide/FirstSide.js
@@ -40,13 +40,14 @@ const FirstSide = () => {
                 <h1 className="text-sm font-bold text-indigo-500">Body background color</h1>
             </div>
 
-            <HexColorPicker color={color} onChange={setColor} />
-
             <div onClick={onColorButtonClickFirst} className="flex flex-row mt-5 bg-gray-200 py-[10px] rounded-sm items-center border border-gray-400 border-1 hover:bg-gray-300 cursor-pointer transition duration-150 ease-in-out">
                 <div className="">
                     <div className="h-7 w-7 bg-red-500 rounded-full mx-3 p-1 border border-2 rounded-full border-white border-opacity-100"/>
                 </div>
                 <p className="text-gray-800 text-sm" >{color}</p>
+
+                {/*<HexColorPicker color={color} onChange={setColor} />*/}
+
             </div>
         </div>
     );
