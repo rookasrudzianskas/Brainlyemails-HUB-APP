ebc9e96f8b3c2462ce5077083b277c613c99309d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 12:03:10 +0200

    Rokas

--- components/DashboardComponents/partials/header/SearchModal.js
+++ components/DashboardComponents/partials/header/SearchModal.js
@@ -11,7 +11,9 @@ function SearchModal() {
     const trigger = useRef(null);
     const searchContent = useRef(null);
     const searchInput = useRef(null);
+    const setImmediate  = () => {
 
+    }
     // close on click outside
     useEffect(() => {
         const clickHandler = ({ target }) => {
@@ -78,7 +80,7 @@ function SearchModal() {
                     <form className="border-b border-gray-200">
                         <div className="relative">
                             <label htmlFor="modal-search" className="sr-only">Search</label>
-                            <input id="modal-search" className="w-full border-0 focus:ring-transparent placeholder-gray-400 appearance-none py-3 pl-10 pr-4" type="search" placeholder="Search Anything…" ref={searchInput} />
+                            <input id="modal-search" className="w-full border-0 focus:ring-transparent placeholder-gray-400 appearance-none py-3 pl-10 pr-4 outline-none" type="search" placeholder="Search Anything…" ref={searchInput} />
                             <button className="absolute inset-0 right-auto group" type="submit" aria-label="Search">
                                 <svg className="w-4 h-4 shrink-0 fill-current text-gray-400 group-hover:text-gray-500 ml-4 mr-2" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
