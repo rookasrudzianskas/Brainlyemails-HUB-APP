1de3567dba54641f6087ec462ea1c83acfa809e2
Rokas <rookas.rudzenskas@gmail.com>
2021-12-15 11:59:55 +0200

    Rokas

--- components/DashboardComponents/partials/header/SearchModal.js
+++ components/DashboardComponents/partials/header/SearchModal.js
@@ -38,7 +38,7 @@ function SearchModal() {
             <button
                 ref={trigger}
                 className={`w-8 h-8 flex items-center justify-center bg-gray-100 hover:bg-gray-200 transition duration-150 rounded-full ml-3 ${searchOpen && 'bg-gray-200'}`}
-                // onClick={() => { setSearchOpen(!searchOpen); setImmediate(() => searchInput.current.focus()); }}
+                onClick={() => { setSearchOpen(!searchOpen); setImmediate(() => searchInput.current.focus()); }}
                 aria-controls="search-modal"
             >
                 <span className="sr-only">Search</span>
@@ -94,14 +94,16 @@ function SearchModal() {
                             <ul className="text-sm">
                                 <li>
                                     {/*<Link*/}
-                                    {/*    className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group"*/}
+                                    {/*    className=""*/}
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Form Builder - 23 hours on-demand video</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -110,10 +112,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Access Mosaic on mobile and TV</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -122,10 +126,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Product Update - Q4 2021</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -134,10 +140,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Master Digital Marketing Strategy course</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -146,10 +154,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Dedicated forms for products</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -158,10 +168,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
+                                    <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
                                         <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
                                             <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
                                         </svg>
                                         <span>Product Update - Q4 2021</span>
+                                    </div>
                                     {/*</Link>*/}
                                 </li>
                             </ul>
@@ -172,14 +184,16 @@ function SearchModal() {
                             <ul className="text-sm">
                                 <li>
                                     {/*<Link*/}
-                                    {/*    className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group"*/}
+                                    {/*    className=""*/}
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
-                                        <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
-                                            <path d="M14 0H2c-.6 0-1 .4-1 1v14c0 .6.4 1 1 1h8l5-5V1c0-.6-.4-1-1-1zM3 2h10v8H9v4H3V2z" />
-                                        </svg>
-                                        <span><span className="font-medium text-gray-800 group-hover:text-white">Messages</span> - Conversation / … / Mike Mills</span>
+                                        <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
+                                            <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
+                                                <path d="M14 0H2c-.6 0-1 .4-1 1v14c0 .6.4 1 1 1h8l5-5V1c0-.6-.4-1-1-1zM3 2h10v8H9v4H3V2z" />
+                                            </svg>
+                                            <span><span className="font-medium text-gray-800 group-hover:text-white">Messages</span> - Conversation / … / Mike Mills</span>
+                                        </div>
                                     {/*</Link>*/}
                                 </li>
                                 <li>
@@ -188,10 +202,12 @@ function SearchModal() {
                                     {/*    to="#0"*/}
                                     {/*    onClick={() => setSearchOpen(!searchOpen)}*/}
                                     {/*>*/}
-                                        <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
-                                            <path d="M14 0H2c-.6 0-1 .4-1 1v14c0 .6.4 1 1 1h8l5-5V1c0-.6-.4-1-1-1zM3 2h10v8H9v4H3V2z" />
-                                        </svg>
-                                        <span><span className="font-medium text-gray-800 group-hover:text-white">Messages</span> - Conversation / … / Eva Patrick</span>
+                                        <div className="flex items-center p-2 text-gray-800 hover:text-white hover:bg-indigo-500 rounded group">
+                                            <svg className="w-4 h-4 fill-current text-gray-400 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
+                                                <path d="M14 0H2c-.6 0-1 .4-1 1v14c0 .6.4 1 1 1h8l5-5V1c0-.6-.4-1-1-1zM3 2h10v8H9v4H3V2z" />
+                                            </svg>
+                                            <span><span className="font-medium text-gray-800 group-hover:text-white">Messages</span> - Conversation / … / Eva Patrick</span>
+                                        </div>
                                     {/*</Link>*/}
                                 </li>
                             </ul>
--- components/DashboardComponents/partials/header/UserMenu.js
+++ components/DashboardComponents/partials/header/UserMenu.js
@@ -40,9 +40,9 @@ function UserMenu() {
                 onClick={() => setDropdownOpen(!dropdownOpen)}
                 aria-expanded={dropdownOpen}
             >
-                <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
+                <img className="w-8 h-8 rounded-full" src={'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg'} width="32" height="32" alt="User" />
                 <div className="flex items-center truncate">
-                    <span className="truncate ml-2 text-sm font-medium group-hover:text-gray-800">Acme Inc.</span>
+                    <span className="truncate ml-2 text-sm font-medium group-hover:text-gray-800">Brainlyemails Inc.</span>
                     <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400" viewBox="0 0 12 12">
                         <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                     </svg>
@@ -65,7 +65,7 @@ function UserMenu() {
                     onBlur={() => setDropdownOpen(false)}
                 >
                     <div className="pt-0.5 pb-2 px-3 mb-1 border-b border-gray-200">
-                        <div className="font-medium text-gray-800">Acme Inc.</div>
+                        <div className="font-medium text-gray-800">Brainlyemails Inc.</div>
                         <div className="text-xs text-gray-500 italic">Administrator</div>
                     </div>
                     <ul>
@@ -75,16 +75,20 @@ function UserMenu() {
                             {/*    to="/"*/}
                             {/*    onClick={() => setDropdownOpen(!dropdownOpen)}*/}
                             {/*>*/}
+                            <div className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3">
                                 Settings
+                            </div>
                             {/*</Link>*/}
                         </li>
                         <li>
                             {/*<Link*/}
-                            {/*    className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3"*/}
+                            {/*    className=""*/}
                             {/*    to="/"*/}
                             {/*    onClick={() => setDropdownOpen(!dropdownOpen)}*/}
                             {/*>*/}
+                            <div className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3">
                                 Sign Out
+                            </div>
                             {/*</Link>*/}
                         </li>
                     </ul>
