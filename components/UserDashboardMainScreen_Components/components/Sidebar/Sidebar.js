56d6635667f8e85e313a8bb1b95d3c522678be17
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 16:33:31 +0200

    Rokas

--- components/UserDashboardMainScreen_Components/components/Sidebar/Sidebar.js
+++ components/UserDashboardMainScreen_Components/components/Sidebar/Sidebar.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import {OVERVIEW} from "../../../../data/ListOfFeatures/Overview";
+import {Icon} from "@mui/material";
+import FeatureList from "./components/FeatureList";
 
 const Sidebar = () => {
     return (
-        <div className="flex max-w-2xl bg-red-500 h-screen">
-            Rokas
-            Rokas
-            Rokas
-            Rokas
-            Rokas
-            Rokas
+        <div className="flex max-w-2xl h-screen">
+           <FeatureList />
         </div>
     );
 };
---
+++ components/UserDashboardMainScreen_Components/components/Sidebar/components/FeatureList/FeatureList.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Feature from "../Feature";
+import GridViewIcon from '@mui/icons-material/GridView';
+
+const FeatureList = () => {
+    return (
+        <div className="">
+            <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                <GridViewIcon />
+                <p className="text-md ml-2">Overview</p>
+            </div>
+
+            <div className="mt-10">
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+            </div>
+
+
+            <div className="mt-10">
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+            </div>
+
+            <div className="mt-10">
+                <div className="flex flex-row py-2 pl-4 pr-20 rounded-sm hover:bg-gray-200 cursor-pointer transition duration-150 ease-in-out">
+                    <GridViewIcon />
+                    <p className="text-md ml-2">Overview</p>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default FeatureList;
