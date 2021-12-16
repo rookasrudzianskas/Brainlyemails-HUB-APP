a5247784f227dd3602fd618994d7e72ca7b25a3b
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 20:17:51 +0200

    Rokas

--- components/UserDashboardMainScreen_Components/components/Center/components/EmailsChart/EmailsChart.js
+++ components/UserDashboardMainScreen_Components/components/Center/components/EmailsChart/EmailsChart.js
@@ -9,9 +9,9 @@ const EmailsChart = () => {
     };
 
     return (
-        <div className="bg-white mt-5 py-10 border border-1 border-indigo-500 rounded-sm">
-            <div className="grid px-10 grid-cols-4 ">
-                <div className="">
+        <div className="bg-white mt-5 py-10 border border-1 border-indigo-500 rounded-sm  px-10">
+            <div className="flex">
+                <div className="flex flex-1">
                     <div className="flex flex-row items-center justify-center">
                         <div className="flex flex-col">
                             <p  className="text-sm text-gray-400 mb-2">Emails sent</p>
@@ -20,7 +20,7 @@ const EmailsChart = () => {
                     </div>
                 </div>
 
-                <div className="">
+                <div className="flex flex-1">
                     <div className="flex flex-row items-center justify-center">
                         <div className="flex flex-col">
                             <p  className="text-sm text-gray-400 mb-2">Opens</p>
@@ -33,7 +33,7 @@ const EmailsChart = () => {
                     </div>
                 </div>
 
-                <div className="">
+                <div className="flex flex-1">
                     <div className="flex flex-row items-center justify-center">
                         <div className="flex flex-col">
                             <p  className="text-sm text-gray-400 mb-2">Bounces</p>
@@ -46,7 +46,7 @@ const EmailsChart = () => {
                     </div>
                 </div>
 
-                <div className="">
+                <div className="flex flex-1">
                     <div className="flex flex-row items-center justify-center">
                         <div className="flex flex-col flex-1">
                             <p  className="text-sm text-gray-400 mb-2">Complaints</p>
@@ -57,8 +57,8 @@ const EmailsChart = () => {
                             </p>
                         </div>
 
-                        <div className="relative inline-block text-left">
-                            <div>
+                        <div className="relative inline-block text-left pl-10">
+                            <div className="">
                                 <button
                                     type="button"
                                     className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
@@ -67,7 +67,7 @@ const EmailsChart = () => {
                                     aria-expanded="true"
                                     aria-haspopup="true"
                                 >
-                                    All Emails
+                                    Emails
                                     <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
@@ -87,14 +87,9 @@ const EmailsChart = () => {
                             {show && (
                                 <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                                     <div className="py-1" role="none">
-                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Account settings</a>
-                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Support</a>
-                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">License</a>
-                                        <form method="POST" action="#" role="none">
-                                            <button type="submit" className="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">
-                                                Sign out
-                                            </button>
-                                        </form>
+                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">API</a>
+                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Messenger</a>
+                                        <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">Automation</a>
                                     </div>
                                 </div>
                             )}
