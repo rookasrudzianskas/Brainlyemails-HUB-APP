8fe0a97f7135d94f7b4e390fc933c4263c0bad2a
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 17:25:28 +0200

    Rokas

--- components/UserDashboardMainScreen_Components/components/Sidebar/SeparateMenuItems/SeparateMenuItems/components/Overview/Overview.js
+++ components/UserDashboardMainScreen_Components/components/Sidebar/SeparateMenuItems/SeparateMenuItems/components/Overview/Overview.js
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import GridViewIcon from "@mui/icons-material/GridView";
+import {useRecoilState} from "recoil";
+import {selectedButton} from "../../../../../../../../contentManagement/atoms/SelectedButton/SelectedButton";
 
 const Overview = () => {
     const [clicked, setClicked] = useState(true);
+    const [alreadySelected, setAlreadySelected] = useRecoilState(selectedButton);
+
     const handleStyles = () => {
         if(clicked === false) {
             setClicked(true);
@@ -10,6 +14,18 @@ const Overview = () => {
             setClicked(false);
         }
     }
+
+    // useEffect(() => {
+    //     if(alreadySelected === null) {
+    //         setAlreadySelected(false);
+    //     }
+    //
+    //     if(alreadySelected === false) {
+    //         handleStyles();
+    //     }
+    //
+    // }, [alreadySelected]);
+
 
     // console.log(clicked)
 
