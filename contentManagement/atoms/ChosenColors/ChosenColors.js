488aa4ee7535a24be6a83604f3295a1349a94523
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 10:32:07 +0200

    Rokas

---
+++ contentManagement/atoms/ChosenColors/ChosenColors.js
@@ -0,0 +1,47 @@
+import {atom} from "recoil";
+
+export const bodyBackgroundColor = atom({
+    key: 'bodyBackgroundColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const highlightColor = atom({
+    key: 'highlightColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const contentBackgroundColor = atom({
+    key: 'contentBackgroundColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const contentBorderColor = atom({
+    key: 'contentBorderColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const buttonTextColor = atom({
+    key: 'buttonTextColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const textColor = atom({
+    key: 'textColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const titleTextColor = atom({
+    key: 'titleTextColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const mutedTextColor = atom({
+    key: 'mutedTextColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+export const footerTextColor = atom({
+    key: 'footerTextColor', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
