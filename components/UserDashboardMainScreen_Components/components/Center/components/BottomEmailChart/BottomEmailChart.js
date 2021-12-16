4ab8289311f523eb84219b417b7bde9ce05649b4
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 20:49:20 +0200

    Rokas

--- components/UserDashboardMainScreen_Components/components/Center/components/BottomEmailChart/BottomEmailChart.js
+++ components/UserDashboardMainScreen_Components/components/Center/components/BottomEmailChart/BottomEmailChart.js
@@ -4,49 +4,46 @@ import CheckIcon from "@mui/icons-material/Check";
 
 const BottomEmailChart = () => {
     return (
-        <div className="bg-white mt-5 py-10 border border-1 border-indigo-500 rounded-sm">
-            <div className="grid px-10 grid-cols-3 ">
-                <div className="">
-                    <div className="flex flex-row items-center justify-center">
-                        <div className="flex flex-col">
-                            <p  className="text-sm text-gray-400 mb-2">Emails sent</p>
-                            <p  className="text-xl">0</p>
+        <div className="bg-white mt-5 py-10 border border-1 border-indigo-500 rounded-sm ">
+            <div className="flex flex-row px-10 space-x-10">
+                <div>
+                    <div className="flex w-[17rem] border-b-[1px] border-indigo-500">
+                        <div className="mb-8">
+                            <div className="flex flex-col items-center">
+                                <p className="text-sm text-gray-400 mb-2">Subscribers</p>
+                                <div className="flex flex-row items-center space-x-2">
+                                    <p className="text-xl">1</p>
+                                    <ShowChartIcon style={{color: '#97EB97', fontSize: 20}} />
+                                    <p style={{color: '#97EB97'}} >1</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <div className="flex flex-row items-center  mt-5">
+                            <div className="flex flex-1">
+                                <p className="text-sm text-gray-400">Subscribes</p>
+                            </div>
+                            <div className="flex">
+                                <p className="text-sm text-gray-900">1</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex flex-row items-center mt-3">
+                                <div className="flex flex-1">
+                                    <p className="text-sm text-gray-400">Unsubscribes</p>
+                                </div>
+                                <div className="flex">
+                                    <p className="text-sm text-gray-900">0</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="">
-                    <div className="flex flex-col items-center border-r-2 border-l-2 border-indigo-500">
-                        <p className="text-sm text-gray-400 mb-2">Subscribers</p>
-                        <div className="flex flex-row items-center space-x-2">
-                            <p className="text-xl">1</p>
-                            <ShowChartIcon style={{color: '#97EB97', fontSize: 20}} />
-                            <p style={{color: '#97EB97'}} >1</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="bg flex items-center justify-center">
-                    <div className="flex flex-col">
-                        <div className="flex flex-row mb-2">
-                            <div className="flex flex-1 mr-16">
-                                <p className="text-sm text-gray-400">Bounce rate</p>
-                            </div>
-                            <div className="flex flex-row items-center px-2 rounded-full" style={{backgroundColor: '#97EB97'}}>
-                                <CheckIcon style={{color: '#1B7120', fontWeight: 500, fontSize: 14}} />
-                                <p style={{color: '#1B7120', fontWeight: 500, fontSize: 12}}>00.00%</p>
-                            </div>
-                        </div>
-                        <div className="flex flex-row">
-                            <div className="flex flex-1 mr-16">
-                                <p className="text-sm text-gray-400">Complaint rate</p>
-                            </div>
-                            <div className="flex flex-row items-center px-2 rounded-full" style={{backgroundColor: '#97EB97'}}>
-                                <CheckIcon style={{color: '#1B7120', fontWeight: 500, fontSize: 14}} />
-                                <p style={{color: '#1B7120', fontWeight: 500, fontSize: 12}}>00.00%</p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="flex w-[40rem] bg-blue-500">
+                    Rokas
                 </div>
             </div>
         </div>
