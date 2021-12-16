b3b3ecc80d99748806e0fbc8d8583e34384efa29
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 17:47:27 +0200

    Rokas

--- components/UserDashboardMainScreen_Components/components/Center/components/SelectTime/SelectTime.js
+++ components/UserDashboardMainScreen_Components/components/Center/components/SelectTime/SelectTime.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SelectTime = () => {
+    const [selected, setSelected] = useState('first');
+
+    const handleClick = () => {
+        if(selected === 'first') {
+            setSelected('second');
+        } else {
+            setSelected('first');
+        }
+    }
+
     return (
         <div className="flex w-full justify-center">
             <div className="flex flex-1"/>
             <div className="flex space-x-2">
-                <p className="text-sm text-gray-800">30 days</p>
-                <p className="text-sm text-gray-800">24 hours</p>
+                <p onClick={handleClick} className={`text-sm ${selected === 'first' ? 'text-gray-800' : 'text-gray-400'} hover:opacity-80 cursor-pointer transition duration-150`}>30 days</p>
+                <p onClick={handleClick} className={`text-sm ${selected === 'second' ? 'text-gray-800' : 'text-gray-400'} hover:opacity-80 cursor-pointer transition duration-150`}>24 hours</p>
             </div>
         </div>
     );
