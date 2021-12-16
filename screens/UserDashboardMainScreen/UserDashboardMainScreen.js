c1594281e90fa7ec01641674160b582dd63a0167
Rokas <rookas.rudzenskas@gmail.com>
2021-12-16 16:10:47 +0200

    Rokas

--- screens/UserDashboardMainScreen/UserDashboardMainScreen.js
+++ screens/UserDashboardMainScreen/UserDashboardMainScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "../../components/DashboardComponents/partials/Header";
 import TopPart from "../../components/UserDashboardMainScreen_Components/TopPart";
+import Footer from "../../components/WelcomeScreen_Components/Footer/Footer";
 
 const UserDashboardMainScreen = () => {
     return (
         <div className="overflow-y-scroll scrollbar-hide">
             <Header />
             <TopPart />
+
+
+            <Footer />
         </div>
     );
 };
