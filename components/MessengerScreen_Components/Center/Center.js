1186741b54f303121994da0e6cdf792b5f544115
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 10:24:23 +0200

    Rokas

--- components/MessengerScreen_Components/Center/Center.js
+++ components/MessengerScreen_Components/Center/Center.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AllFeaturesContainer from "../AllFeaturesContainer";
+import NewMessage from "../AllFeaturesContainer/components/NewMessage";
 
 const Center = () => {
     return (
-        <div className="flex flex-1  bg-transparent h-screen">
-
+        <div className="flex flex-1 flex-col bg-transparent h-screen">
+            <AllFeaturesContainer />
+            <NewMessage />
         </div>
     );
 };
---
+++ data/FakeEmails/FakeEmails.js
@@ -0,0 +1,12 @@
+export const FAKE_EMAILS = [
+    {
+        title: '(no subject)',
+        body: '',
+        isDraft: true,
+    },
+    {
+        title: 'Welcome to Messenger',
+        body: 'Messenger is the easiest way to let your audience/customers know what you\'re up to. Send newsletters, product updates, follow‑ups, you name it. ',
+        isDraft: true,
+    },
+]
