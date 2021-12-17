83077e4c19a800121661830a019a2a32c2bce759
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 12:47:15 +0200

    Rokas

--- components/NewEmailScreen_Components/EmailFeaturesContainer/Sidebar/components/FeaturesList/FeaturesList.js
+++ components/NewEmailScreen_Components/EmailFeaturesContainer/Sidebar/components/FeaturesList/FeaturesList.js
@@ -6,24 +6,24 @@ import TodayIcon from '@mui/icons-material/Today';
 
 const FeaturesList = () => {
     return (
-        <div className="mx-3 space-y-3 mt-7">
-            <div className="flex flex-row bg-gray-100 ">
-                <DeleteIcon className="text-gray-400 mr-3" />
+        <div className="mx-3 space-y-2 mt-7">
+            <div className="flex flex-row py-2 px-2 hover:bg-gray-300 hover:text-gray-800 transition duration-150 ease-in-out rounded-sm cursor-pointer ">
+                <DeleteIcon className="text-gray-400 mr-3 hover:text-gray-800" />
                 <p className="text-gray-600">Discard draft</p>
             </div>
 
-            <div className="flex flex-row">
-                <MoreHorizIcon className="text-gray-400 mr-3" />
+            <div className="flex flex-row py-2 px-2 hover:bg-gray-300 hover:text-gray-800 transition duration-150 ease-in-out rounded-sm cursor-pointer ">
+                <MoreHorizIcon className="text-gray-400 mr-3 hover:text-gray-800" />
                 <p className="text-gray-600">More options...</p>
             </div>
 
-            <div className="flex flex-row">
-                <VisibilityIcon className="text-gray-400 mr-3" />
+            <div className="flex flex-row py-2 px-2 hover:bg-gray-300 hover:text-gray-800 transition duration-150 ease-in-out rounded-sm cursor-pointer ">
+                <VisibilityIcon className="text-gray-400 mr-3 hover:text-gray-800" />
                 <p className="text-gray-600">Show email preview</p>
             </div>
 
-            <div className="flex flex-row">
-                <TodayIcon className="text-gray-400 mr-3" />
+            <div className="flex flex-row py-2 px-2 hover:bg-gray-300 hover:text-gray-800 transition duration-150 ease-in-out rounded-sm cursor-pointer ">
+                <TodayIcon className="text-gray-400 mr-3 hover:text-gray-800" />
                 <p className="text-gray-600">Schedule delivery</p>
             </div>
         </div>
