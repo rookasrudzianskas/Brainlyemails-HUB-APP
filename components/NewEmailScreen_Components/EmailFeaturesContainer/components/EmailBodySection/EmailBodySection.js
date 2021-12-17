acb614f0e3ffd235f0dd1ceccad1a9106df0b8d2
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 14:12:25 +0200

    Rokas

--- components/NewEmailScreen_Components/EmailFeaturesContainer/components/EmailBodySection/EmailBodySection.js
+++ components/NewEmailScreen_Components/EmailFeaturesContainer/components/EmailBodySection/EmailBodySection.js
@@ -2,49 +2,14 @@ import React, {useCallback} from 'react';
 // Import React dependencies.
 import { useMemo, useState } from 'react'
 // Import the Slate editor factory.
-import { createEditor } from 'slate'
+import { createEditor } from 'slate';
+import CodeIcon from '@mui/icons-material/Code';
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react';
 import { Editor, Transforms } from 'slate';
+import {CustomEditor} from "../SubjectSection/utils/CustomEditor";
 
-// Define our own custom set of helpers.
-const CustomEditor = {
-    isBoldMarkActive(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: n => n.bold === true,
-            universal: true,
-        })
-
-        return !!match
-    },
-
-    isCodeBlockActive(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: n => n.type === 'code',
-        })
-
-        return !!match
-    },
-
-    toggleBoldMark(editor) {
-        const isActive = CustomEditor.isBoldMarkActive(editor)
-        Transforms.setNodes(
-            editor,
-            { bold: isActive ? null : true },
-            { match: n => Text.isText(n), split: true }
-        )
-    },
-
-    toggleCodeBlock(editor) {
-        const isActive = CustomEditor.isCodeBlockActive(editor)
-        Transforms.setNodes(
-            editor,
-            { type: isActive ? null : 'code' },
-            { match: n => Editor.isBlock(editor, n) }
-        )
-    },
-}
 
 const EmailBodySection = () => {
     const editor = useMemo(() => withReact(createEditor()), [])
@@ -74,48 +39,58 @@ const EmailBodySection = () => {
         <div className="px-10 py-5 h-[10rem]">
             <div className="flex flex-row items-center">
                 <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-                    <div>
-                        <button
-                            onMouseDown={event => {
-                                event.preventDefault()
-                                CustomEditor.toggleBoldMark(editor)
-                            }}
-                        >
-                            Bold
-                        </button>
-                        <button
-                            onMouseDown={event => {
-                                event.preventDefault()
-                                CustomEditor.toggleCodeBlock(editor)
-                            }}
-                        >
-                            Code Block
-                        </button>
-                    </div>
-                    <Editable
-                        editor={editor}
-                        renderElement={renderElement}
-                        renderLeaf={renderLeaf}
-                        onKeyDown={event => {
-                            if (!event.ctrlKey) {
-                                return
-                            }
-
-                            switch (event.key) {
-                                case '`': {
-                                    event.preventDefault()
-                                    CustomEditor.toggleCodeBlock(editor)
-                                    break
-                                }
-
-                                case 'b': {
+                    <div className="flex flex-col">
+                        <div className="flex items-center space-x-2">
+                            <button
+                                onMouseDown={event => {
                                     event.preventDefault()
                                     CustomEditor.toggleBoldMark(editor)
-                                    break
-                                }
-                            }
-                        }}
-                    />
+                                }}
+                            >
+                                <div className="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-sm mb-3">
+                                    <p className="text-xl font-semibold">
+                                        B
+                                    </p>
+                                </div>
+                            </button>
+                            <button
+                                onMouseDown={event => {
+                                    event.preventDefault()
+                                    CustomEditor.toggleCodeBlock(editor)
+                                }}
+                            >
+                                <div className="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-sm mb-3">
+                                    <CodeIcon />
+                                </div>
+                            </button>
+                        </div>
+                        <div>
+                            <Editable
+                                editor={editor}
+                                renderElement={renderElement}
+                                renderLeaf={renderLeaf}
+                                onKeyDown={event => {
+                                    if (!event.ctrlKey) {
+                                        return
+                                    }
+
+                                    switch (event.key) {
+                                        case '`': {
+                                            event.preventDefault()
+                                            CustomEditor.toggleCodeBlock(editor)
+                                            break
+                                        }
+
+                                        case 'b': {
+                                            event.preventDefault()
+                                            CustomEditor.toggleBoldMark(editor)
+                                            break
+                                        }
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
                 </Slate>
             </div>
         </div>
