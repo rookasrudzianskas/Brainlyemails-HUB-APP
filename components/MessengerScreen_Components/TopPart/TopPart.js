5386ee3576268b4acc84158103e099f30cf4a477
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 10:50:39 +0200

    Rokas

--- components/MessengerScreen_Components/TopPart/TopPart.js
+++ components/MessengerScreen_Components/TopPart/TopPart.js
@@ -5,7 +5,7 @@ import Center from "../Center";
 const TopPart = () => {
     return (
         <div className="">
-            <div className=" bg-gray-100 h-[85rem]">
+            <div className=" bg-gray-100 h-[55rem]">
                 <div className="container mx-auto px-4 sm:px-8 max-w-7xl">
                     <div className="flex flex-row items-center">
                         <div className="flex flex-row flex-1 mt-16">
