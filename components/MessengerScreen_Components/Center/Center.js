a95baca89c1e9ab555a3b86af29b1511fcbf75f1
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 10:46:18 +0200

    Rokas

--- components/MessengerScreen_Components/Center/Center.js
+++ components/MessengerScreen_Components/Center/Center.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AllFeaturesContainer from "../AllFeaturesContainer";
 import NewMessage from "../AllFeaturesContainer/components/NewMessage";
+import EmailsSection from "../AllFeaturesContainer/components/EmailsSection";
 
 const Center = () => {
     return (
         <div className="flex flex-1 flex-col bg-transparent h-screen">
             <AllFeaturesContainer />
             <NewMessage />
+            <EmailsSection />
         </div>
     );
 };
