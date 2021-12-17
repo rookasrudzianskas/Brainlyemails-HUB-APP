5ca299dcc7306d44b0d8498a35c037ddca834e23
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 18:07:10 +0200

    Rokas

---
+++ data/SendingTimes/SendingTimes.js
@@ -0,0 +1,17 @@
+export const TIMES = [
+    {
+        day: 'Tomorrow',
+        time: '8:00 AM',
+        date: '18 Dec 2021,',
+    },
+    {
+        day: 'Tomorrow',
+        time: '2:00 PM',
+        date: '18 Dec 2021,',
+    },
+    {
+        day: 'Monday',
+        time: '8:00 AM',
+        date: '20 Dec 2021,',
+    },
+]
