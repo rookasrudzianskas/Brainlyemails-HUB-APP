091abc071cf87d5acf7c15d72b92b00150c6ddbd
Rokas <rookas.rudzenskas@gmail.com>
2021-12-17 16:42:46 +0200

    Rokas

--- components/NewEmailScreen_Components/EmailFeaturesContainer/Center/Center.js
+++ components/NewEmailScreen_Components/EmailFeaturesContainer/Center/Center.js
@@ -6,17 +6,19 @@ import EmailBodySection from "../components/EmailBodySection";
 import {handleTextEditorAndTemplate} from "../../../../contentManagement/atoms/HandleTextEditorAndTemplateState/HandleTextEditorAndTemplateState";
 import {useRecoilState} from "recoil";
 import ChooseASpecificEmailTemplate from "../components/ChooseASpecificEmailTemplate";
+import {handleShowEmailPreviewModal} from "../../../../contentManagement/atoms/HandleModalStates/HandleModalStates";
 
 const Center = () => {
     const [handleTextEditorAndTemplateState, setHandleTextEditorAndTemplateState] = useRecoilState(handleTextEditorAndTemplate);
+    const [handleShowEmailPreviewModalState, setHandleShowEmailPreviewModalState] = useRecoilState(handleShowEmailPreviewModal);
 
     return (
         <div className="flex w-[50rem] flex flex-col bg-white border border-1 border-indigo-500 rounded-sm">
             <FromSection />
             <ToSection />
             <SubjectSection />
-            {!handleTextEditorAndTemplateState && <EmailBodySection />}
-            {handleTextEditorAndTemplateState && <ChooseASpecificEmailTemplate />}
+            {!handleTextEditorAndTemplateState || !handleShowEmailPreviewModalState && <EmailBodySection />}
+            {handleTextEditorAndTemplateState || handleShowEmailPreviewModalState && <ChooseASpecificEmailTemplate />}
         </div>
     );
 };
