e772f04141b724740fbdbd5a0dd8e720bd342fe9
Rokas <rookas.rudzenskas@gmail.com>
2021-12-18 10:39:41 +0200

    Rokas

--- components/NewAutomationlScreen_Components/AutomationFeaturesContainer/components/AddMessages/AddMessages.js
+++ components/NewAutomationlScreen_Components/AutomationFeaturesContainer/components/AddMessages/AddMessages.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 const AddMessages = () => {
     return (
-        <div className="flex w-full flex flex-col bg-white border border-1 border-indigo-500 rounded-sm px-10 py-12">
-            <div className="max-w-5xl">
-                <p className="text-xl text-gray-800">
-                    Messages
-                </p>
-                <p className="text-gray-500 mt-3">
-                    Add messages to your email sequence and specify a time delay for each message before it gets sent.
-                </p>
+        <div className="flex w-full flex flex-col bg-white border border-1 border-indigo-500 rounded-sm ">
+            <div className="px-10 pt-12">
+                <div className="max-w-5xl">
+                    <p className="text-xl text-gray-800">
+                        Messages
+                    </p>
+                    <p className="text-gray-500 mt-3">
+                        Add messages to your email sequence and specify a time delay for each message before it gets sent.
+                    </p>
+                </div>
+
+            </div>
+
+            <div className="bg-indigo-100 py-10 mt-6 flex items-center justify-center">
+                <div className="flex">
+                    <p className="flex text-indigo-500 hover:underline cursor-pointer transition duration-150 ease-in-out">+ Add message</p>
+                </div>
             </div>
         </div>
     );
