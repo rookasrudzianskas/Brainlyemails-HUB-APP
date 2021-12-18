70f6357531e96313ee3006afbf63fc52119bda8d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-18 12:15:49 +0200

    Rokas

--- components/NewAutomationlScreen_Components/NewAutomatedEmailScreen_Components/AutomatedEmailFeaturesContainer/Sidebar/components/PreviewAndSend/PreviewAndSend.js
+++ components/NewAutomationlScreen_Components/NewAutomatedEmailScreen_Components/AutomatedEmailFeaturesContainer/Sidebar/components/PreviewAndSend/PreviewAndSend.js
@@ -4,7 +4,7 @@ const PreviewAndSend = () => {
     return (
         <div className="flex flex-col mt-5">
             <div className="flex flex-row justify-center items-center bg-indigo-500 py-2 px-3 rounded-sm hover:bg-indigo-400 cursor-pointer transition duration-150 ease-in-out">
-                <p className="text-gray-100">Preview & send</p>
+                <p className="text-gray-100">Add message to sequence</p>
             </div>
 
             <div className="mt-2">
