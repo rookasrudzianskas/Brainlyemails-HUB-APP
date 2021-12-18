68b9fa46b40e306d7ac0011010c4c64d37c5c361
Rokas <rookas.rudzenskas@gmail.com>
2021-12-18 12:12:46 +0200

    Rokas

--- screens/NewAutomatedEmailScreen/NewAutomatedEmailScreen.js
+++ screens/NewAutomatedEmailScreen/NewAutomatedEmailScreen.js
@@ -3,13 +3,22 @@ import Header from "../../components/DashboardComponents/partials/Header";
 import Footer from "../../components/WelcomeScreen_Components/Footer/Footer";
 import TopPart
     from "../../components/NewAutomationlScreen_Components/NewAutomatedEmailScreen_Components/AutomatedEmailFeaturesContainer/TopPart";
+import {useRecoilState} from "recoil";
+import {handleShowEmailPreviewModal} from "../../contentManagement/atoms/HandleModalStates/HandleModalStates";
+import EmailPreviewDraftModal from "../../components/Modals/EmailPreviewDraftModal";
 
 const NewAutomatedEmailScreen = () => {
+    const [handleShowEmailPreviewModalState, setHandleShowEmailPreviewModalState] = useRecoilState(handleShowEmailPreviewModal);
+
     return (
         <div className="">
-            <Header />
+            {!handleShowEmailPreviewModalState && <Header/>}
+            {/*<Header />*/}
             <TopPart />
-            <Footer />
+            {handleShowEmailPreviewModalState && (
+                <EmailPreviewDraftModal />
+            )}
+            {!handleShowEmailPreviewModalState && <Footer/>}
         </div>
     );
 };
