d5e642d0cc1f750f98eb00ad6fc72e900319fcf5
Rokas <rookas.rudzenskas@gmail.com>
2021-12-20 22:33:37 +0200

    Rokas

--- contentManagement/atoms/FormDataCloud/FormDataCloud.js
+++ contentManagement/atoms/FormDataCloud/FormDataCloud.js
@@ -4,7 +4,7 @@ export const formDataCloudState = atom({
     key: 'formDataCloudState',
     default: {
         formData: {
-            name: '',
+            name: 'This is epic email',
             formName: 'This is banana form 🍌',
             title: 'This is going to be epic 🙎‍♂️',
             description: 'Never miss a thing from _MAGICAL_. You can subscribe for free.\n We don\'t send spam, and if you get bored, you can unsubscribe with one click.',
