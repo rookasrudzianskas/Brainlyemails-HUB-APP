f519a7600a9627c6d4bf30a59456cc76818d6900
Rokas <rookas.rudzenskas@gmail.com>
2021-12-20 19:01:42 +0200

    Rokas

--- components/EditFormScreen_Components/LeftSide/components/GeneralInputs/GeneralInputs.js
+++ components/EditFormScreen_Components/LeftSide/components/GeneralInputs/GeneralInputs.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GeneralInputs = () => {
+    const [showTheFormattingOptions, setShowTheFormattingOptions] = useState(false);
+
     return (
-        <div className="">
+        <div className="overflow-y-scroll">
             <p className="text-gray-800 text-lg">General</p>
 
 
@@ -17,13 +19,27 @@ const GeneralInputs = () => {
                 </div>
 
                 <div className="mb-4 flex flex-col mt-3">
-                    <p className="text-xs text-indigo-500 mb-1">TEXT</p>
-                    <textarea
-                        className="appearance-none bg-gray-100 rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        style={{border: 'none', resize: 'none'}}
-                        rows="4"
-                        placeholder="Tell something cool..." />
+                    <div className="flex flex-row">
+                        <p className="text-xs text-indigo-500 mb-1 flex flex-1">TEXT</p>
+                        <div onClick={() => setShowTheFormattingOptions(!showTheFormattingOptions)}>
+                            <p className="flex text-xs text-indigo-300 hover:underline cursor-pointer">formatting help</p>
+                        </div>
+                    </div>
+
+                    {showTheFormattingOptions && (
+                        <div>
+                            Rokas
+                        </div>
+                    )}
+
+                    <div>
+                        <textarea
+                            className="appearance-none bg-gray-100 rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            style={{border: 'none', resize: 'none'}}
+                            rows="4"
+                            placeholder="Tell something cool..." />
+                    </div>
                 </div>
 
                 <div className="mb-4 flex flex-col mt-3">
@@ -40,6 +56,28 @@ const GeneralInputs = () => {
 
             <div>
                 <p className="text-gray-800 text-lg">Fields</p>
+            </div>
+
+            <div>
+                <div className="mb-4 flex flex-col mt-3">
+                    <p className="text-xs text-indigo-500 mb-1">TITLE</p>
+                    <input
+                        className="appearance-none bg-gray-100 rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        style={{border: 'none'}}
+                        type="text" placeholder="Email address" />
+                </div>
+
+                <div className="mb-4 flex flex-col mt-3">
+                    <p className="text-xs text-indigo-500 mb-1">CONTACT PROP</p>
+                    <input
+                        className="appearance-none bg-gray-100 rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        style={{border: 'none'}}
+                        type="text" placeholder="Email address"
+                        value="Email address" />
+                </div>
+
             </div>
         </div>
     );
