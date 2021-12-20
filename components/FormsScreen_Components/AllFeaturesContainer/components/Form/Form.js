d2122ec4d5e12fcdc1e1b359fec6990e5379fe3f
Rokas <rookas.rudzenskas@gmail.com>
2021-12-20 17:13:40 +0200

    Rokas

--- components/FormsScreen_Components/AllFeaturesContainer/components/Form/Form.js
+++ components/FormsScreen_Components/AllFeaturesContainer/components/Form/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = ({form}) => {
     return (
         <div className="bg-white mt-5 py-6 px-10 border border-1 border-indigo-500 rounded-sm hover:bg-gray-100 cursor-pointer transition duration-150 ease-in-out">
-            <div className="flex flex-row">
+            <div className="flex flex-row items-center">
                 <div className="flex flex-col">
                     <div>
                         <div className="flex flex-row items-center">
@@ -20,14 +20,15 @@ const Form = ({form}) => {
                     </p>
                 </div>
 
-                <div className="flex flex-col flex-1">
-
+                <div className="flex flex-col flex-1 ml-[10rem] mt-1">
+                    <p className="flex flex-1 text-sm text-gray-800">Views</p>
+                    <p className="flex flex-1 font-bold ml-7">0</p>
                 </div>
 
                 <div className="flex flex-col flex-1">
-                    <div className="flex flex-row">
-                        <p className="flex flex-1">Subscribed</p>
-                        <p className="flex">0%</p>
+                    <div className="flex flex-row mb-2 items-center">
+                        <p className="flex flex-1 text-sm text-gray-800">Subscribed</p>
+                        <p className="flex font-bold">0%</p>
                     </div>
                     <div className="flex flex-col">
                         <div className="w-full bg-gray-200 rounded-full h-1 dark:bg-gray-700">
