77be5c0af90d2f24f93ae33b22196279d75ffaa6
Rokas <rookas.rudzenskas@gmail.com>
2021-12-21 18:36:34 +0200

    Rokas

--- components/BuildAndEditEmailScreen_Components/LeftSide/components/GeneralInputs/components/Text/Text.js
+++ components/BuildAndEditEmailScreen_Components/LeftSide/components/GeneralInputs/components/Text/Text.js
@@ -7,7 +7,14 @@ const Text = () => {
     const [manageInputThingsState, setManageInputThingsState] = useRecoilState(manageInputThings);
 
     return (
-        <div className="px-6 py-3 flex flex-row space-x-2 text-indigo-200 hover:bg-indigo-200 hover:text-indigo-500 cursor-pointer transition duration-150 ease-in-out">
+        <div onClick={(e) =>
+            setManageInputThingsState({
+                ...manageInputThingsState,
+                Text: {
+                    isSelected: !manageInputThingsState.Text.isSelected,
+                }
+            })
+        } className="px-6 py-3 flex flex-row space-x-2 text-indigo-200 hover:bg-indigo-200 hover:text-indigo-500 cursor-pointer transition duration-150 ease-in-out">
             <Notes />
             <p>Text</p>
         </div>
