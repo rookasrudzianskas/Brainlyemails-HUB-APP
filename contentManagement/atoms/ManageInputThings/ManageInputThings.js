78cabd2ef69f1bf5f21020f6218422f01589bf8f
Rokas <rookas.rudzenskas@gmail.com>
2021-12-21 18:33:40 +0200

    Rokas

---
+++ contentManagement/atoms/ManageInputThings/ManageInputThings.js
@@ -0,0 +1,31 @@
+import {atom} from 'recoil';
+
+export const manageInputThings = new atom({
+    key: 'manageInputThings',
+    default: {
+        Text: {
+
+        },
+        Table: {
+
+        },
+        Separator: {
+
+        },
+        List: {
+
+        },
+        Image: {
+
+        },
+        Code: {
+
+        },
+        Chart: {
+
+        },
+        Button: {
+
+        }
+    }
+})
