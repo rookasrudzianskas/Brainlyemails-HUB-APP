878aad7d468cff7e736e25e805e9d542d84e2321
Rokas <rookas.rudzenskas@gmail.com>
2021-12-21 21:43:31 +0200

    Rokas

--- contentManagement/atoms/NewEmailTemplateDataCloud/NewEmailTemplateDataCloud.js
+++ contentManagement/atoms/NewEmailTemplateDataCloud/NewEmailTemplateDataCloud.js
@@ -2,8 +2,8 @@ import {atom} from 'recoil';
 
 export const newEmailTemplateDataCloud = new atom({
     key: 'newEmailTemplateDataCloud',
-   default: {
-       templateName: 'something special',
-       templateSubject: '🍌 Banana email template',
-   },
+    default: {
+        templateName: 'something special',
+        templateSubject: '🍌 Banana email template',
+    },
 });
