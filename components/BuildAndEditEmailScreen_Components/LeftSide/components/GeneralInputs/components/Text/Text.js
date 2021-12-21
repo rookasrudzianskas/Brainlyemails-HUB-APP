7cb32012d4a00835a6a7306d698314f89ed74ba3
Rokas <rookas.rudzenskas@gmail.com>
2021-12-21 18:35:10 +0200

    Rokas

--- components/BuildAndEditEmailScreen_Components/LeftSide/components/GeneralInputs/components/Text/Text.js
+++ components/BuildAndEditEmailScreen_Components/LeftSide/components/GeneralInputs/components/Text/Text.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Notes} from "@mui/icons-material";
+import {manageInputThings} from "../../../../../../../contentManagement/atoms/ManageInputThings/ManageInputThings";
+import {useRecoilState} from "recoil";
 
 const Text = () => {
+    const [manageInputThingsState, setManageInputThingsState] = useRecoilState(manageInputThings);
+
     return (
         <div className="px-6 py-3 flex flex-row space-x-2 text-indigo-200 hover:bg-indigo-200 hover:text-indigo-500 cursor-pointer transition duration-150 ease-in-out">
             <Notes />
--- contentManagement/atoms/ManageInputThings/ManageInputThings.js
+++ contentManagement/atoms/ManageInputThings/ManageInputThings.js
@@ -4,28 +4,28 @@ export const manageInputThings = new atom({
     key: 'manageInputThings',
     default: {
         Text: {
-
+            isSelected: false,
         },
         Table: {
-
+            isSelected: false,
         },
         Separator: {
-
+            isSelected: false,
         },
         List: {
-
+            isSelected: false,
         },
         Image: {
-
+            isSelected: false,
         },
         Code: {
-
+            isSelected: false,
         },
         Chart: {
-
+            isSelected: false,
         },
         Button: {
-
-        }
+            isSelected: false,
+        },
     }
 })
