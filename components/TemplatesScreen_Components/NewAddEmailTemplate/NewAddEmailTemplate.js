5bf04d9471e3a3cd7ee54e29d1f14717f1d2fb09
Rokas <rookas.rudzenskas@gmail.com>
2021-12-21 17:02:01 +0200

    Rokas

--- components/TemplatesScreen_Components/NewAddEmailTemplate/NewAddEmailTemplate.js
+++ components/TemplatesScreen_Components/NewAddEmailTemplate/NewAddEmailTemplate.js
@@ -6,15 +6,57 @@ const NewAddEmailTemplate = () => {
         <div className="flex w-screen h-screen bg-gray-100 flex-col">
             <Header />
 
-            <div className="flex w-full mt-10 items-center justify-center">
-                <div className="bg-white flex flex-col py-7 px-7 max-w-[45rem] w-[45rem]">
+            <div className="flex w-full mt-10 items-center justify-center ">
+                <div className="bg-white flex flex-col py-7 px-7 max-w-[45rem] border-indigo-500 border rounded-sm w-[45rem]">
                     <div className="flex">
                         <h1 className="text-xl text-gray-800">New email template</h1>
                     </div>
 
-                    <div>
+                    <div className="mt-3">
                         <h2 className="text-indigo-400">Create great looking email with no-code email builder.</h2>
                     </div>
+
+
+                    <div className="mt-6">
+                        <p className="text-md text-indigo-500 font-semibold mb-1">Template name</p>
+                        <input
+                            className="shadow-xs rounded-sm appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Password reset template #1"
+                            // value={formDataCloudStateLayer}
+                            // onChange={(e) => setFormDataCloudStateLayer({
+                            //     ...formDataCloudStateLayer,
+                            //     name: e.target.value
+                            // })}
+                        />
+                    </div>
+
+                    <div className="mt-6">
+                        <p className="text-md text-indigo-500 font-semibold mb-1">Default subject line</p>
+                        <input
+                            className="shadow-xs rounded-sm appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Password reset"
+                            // value={formDataCloudStateLayer}
+                            // onChange={(e) => setFormDataCloudStateLayer({
+                            //     ...formDataCloudStateLayer,
+                            //     name: e.target.value
+                            // })}
+                        />
+                    </div>
+
+
+                    <div className="flex flex-row mt-16">
+                        <div className="flex flex-row flex-1">
+                            <p className="text-indigo-300 hover:underline transition duration-150 ease-in-out cursor-pointer">Discard</p>
+                        </div>
+                        <div className="flex flex-row bg-indigo-500 px-2 py-2 rounded-sm hover:bg-indigo-400 transition duration-150 ease-in-out cursor-pointer">
+                            <p className="text-gray-100">Create template</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
---
+++ contentManagement/atoms/NewEmailTemplateDataCloud/NewEmailTemplateDataCloud.js
@@ -0,0 +1,6 @@
+import {atom} from 'recoil';
+
+export const newEmailTemplateDataCloud = new atom({
+    key: 'newEmailTemplateDataCloud',
+   default: 'something very special',
+});
